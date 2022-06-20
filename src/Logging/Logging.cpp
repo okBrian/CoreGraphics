8d75c8082897f755ccb3b951b83e888cef5f1776
Brian Ok <brianok924@gmail.com>
2022-06-20 17:21:19 -0400

    Remade the logger class

--- src/Logging/Logging.cpp
+++ src/Logging/Logging.cpp
@@ -2,21 +2,46 @@
 
 namespace Core
 {
+	// The Core Logging Class that tells info about the core library
+	std::shared_ptr<spdlog::logger> Logging::CoreLogger;
+	// The App Logger which the user can use to debug info
+	std::shared_ptr<spdlog::logger> Logging::AppLogger; 
 
-	void Logging::errorImpl(std::string errorMsg)
+	// Singleton Design Pattern
+	Logging& Logging::getInstance()
 	{
-		spdlog::error(errorMsg);
-		throw(errno);
+		static Logging instance; // Guaranteed to be destroyed.
+								 // Instantiated on first use.
+		return instance;	
 	}
 
-	void Logging::criticalImpl(std::string errorMsg)
+	// Initialize the Logger Class
+	void Logging::init()
 	{
-		spdlog::critical(errorMsg);
-		throw(errno);
+		auto console = spdlog::stdout_color_mt("console");
+		
+		CoreLogger = std::make_shared<spdlog::logger>("CORE");
+		CoreLogger->set_pattern("%^[%n][%l][%@]%$ - %v");
+		spdlog::register_logger(CoreLogger);
+		CoreLogger->set_level(spdlog::level::debug);
+		CoreLogger->flush_on(spdlog::level::debug);
+
+		AppLogger = std::make_shared<spdlog::logger>("APP");
+		AppLogger->set_pattern("%^[%n][%l][%@]%$ - %v");
+		spdlog::register_logger(AppLogger);
+		AppLogger->set_level(spdlog::level::debug);
+		AppLogger->flush_on(spdlog::level::debug);
 	}
 
-	void Logging::warningImpl(std::string warningMsg)
+	// Set the level of the Core Class 
+	void Logging::setCoreLevel(spdlog::level::level_enum level)
 	{
-		spdlog::warn(warningMsg);
+		CoreLogger->set_level(level);
+	}
+
+	// Set the level of the App Class 
+	void Logging::setAppLevel(spdlog::level::level_enum level)
+	{
+		AppLogger->set_level(level);
 	}
 };
--- src/Logging/Logging.hpp
+++ src/Logging/Logging.hpp
@@ -1,29 +1,51 @@
 #pragma once
 #include <spdlog/spdlog.h>
+#include "spdlog/sinks/stdout_color_sinks.h"
 #include <string>
 
 namespace Core
 {
 
-	// Logging: Give user info about errors and warnings 
+	// Logging: Singleton class that gives the user info, warnings, and error messages 
 	class Logging
 	{
-	public:
-		Logging(const Logging&) = delete;
-
-		static Logging& Get()
-		{
-			static Logging _Instance;
-			return _Instance;
-		}
-		
-		static void error(std::string errorMsg) { return Get().errorImpl(errorMsg); }
-		static void critical(std::string errorMsg) { return Get().criticalImpl(errorMsg); }
-		static void warning(std::string warningMsg) { return Get().warningImpl(warningMsg); }
 	private:
-		Logging() {}
-		void errorImpl(std::string errorMsg);
-		void criticalImpl(std::string errorMsg);
-		void warningImpl(std::string warningMsg);
+		// The Core Logging Class that tells info about the core library
+		static std::shared_ptr<spdlog::logger> CoreLogger;
+		// The App Logger which the user can use to debug info
+		static std::shared_ptr<spdlog::logger> AppLogger; 
+	private:
+		// Singleton Design Pattern
+		Logging() {} 
+	public:
+		// Singleton Design Pattern
+		Logging(Logging const&) = delete;
+        void operator=(Logging const&) = delete;
+		static Logging& getInstance();
+		// Actual part of Logging Class
+		// Initialize the Logger Class
+		static void init();
+		// Set the level of the Core Class 
+		static void setCoreLevel(spdlog::level::level_enum level);
+		// Set the level of the App Class 
+		static void setAppLevel(spdlog::level::level_enum level);
+		// Getters used for the preprocessor defines
+		static std::shared_ptr<spdlog::logger>& getCoreLogger() { return CoreLogger; }
+		// Getters used for the preprocessor defines
+		static std::shared_ptr<spdlog::logger>& getAppLogger() { return AppLogger; }
 	};
 }
+
+/* PREPROCESSOR DEFINES FOR EASY ACCESS TO LOGGER */
+
+#define CORE_DEBUG(...)    ::Core::Logging::getCoreLogger()->debug(__VA_ARGS__)
+#define CORE_INFO(...)     ::Core::Logging::getCoreLogger()->info(__VA_ARGS__)
+#define CORE_WARN(...)     ::Core::Logging::getCoreLogger()->warn(__VA_ARGS__)
+#define CORE_ERROR(...)    ::Core::Logging::getCoreLogger()->error(__VA_ARGS__)
+#define CORE_CRITICAL(...) ::Core::Logging::getCoreLogger ()->critical(__VA_ARGS__)
+
+#define APP_DEBUG(...)		   ::Core::Logging::getAppLogger()->debug(__VA_ARGS__)
+#define APP_INFO(...)          ::Core::Logging::getAppLogger()->info(__VA_ARGS__)
+#define APP_WARN(...)          ::Core::Logging::getAppLogger()->warn(__VA_ARGS__)
+#define APP_ERROR(...)         ::Core::Logging::getAppLogger()->error(__VA_ARGS__)
+#define APP_CRITICAL(...)      ::Core::Logging::getAppLogger()->critical(__VA_ARGS__)
